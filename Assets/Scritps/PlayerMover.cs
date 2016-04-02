5c08f2ee6aac2ae5d0baab5894c85ad18dc08bbd
Matthew Collins <kaptain06@live.com>
2016-04-01 21:03:30 -0400

    New First Push

---
+++ Assets/Scritps/PlayerMover.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMover : MonoBehaviour {
+
+	public bool player;
+	public Rigidbody rb;
+	public GameObject self;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		self.transform.Translate (Vector2.right / 20);
+
+
+		if (Input.GetKeyDown (KeyCode.W)) {
+			rb.AddForce (transform.up*500);
+
+		}
+
+		if (this.gameObject.transform.position.x >= 10) {
+			self.transform.position = new Vector2 (-9.5f, this.gameObject.transform.position.y);
+		}
+
+	}
+}

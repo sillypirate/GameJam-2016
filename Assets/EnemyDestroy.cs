a160d4a8a17cef129dad3bad32258be1f3264603
tardead <imagic.miller@gmail.com>
2016-04-02 01:44:55 -0400

    Added files via upload
    Prefabs with Level1 layout.
    New script: EnemyDestroy.cs  add to both players

---
+++ Assets/EnemyDestroy.cs
@@ -0,0 +1,16 @@
+﻿// Brian Miller
+
+using UnityEngine;
+using System.Collections;
+
+public class EnemyDestroy : MonoBehaviour {
+
+	// Destroy Enemy Object
+	void OnCollisionEnter (Collision col)
+	{
+		if(col.gameObject.name == "Enemy")
+		{
+			Destroy (col.gameObject);
+		}
+	}
+}
